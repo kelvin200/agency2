db390f58410d73671a72d16983c5c8e69258fd11
Kelvin <nhnhatquan@gmail.com>
2022-03-04 08:55:20 +0800

    Update

--- clients/athena/apps/admin/code/src/plugins/route.tsx
+++ clients/athena/apps/admin/code/src/plugins/route.tsx
@@ -1,10 +1,18 @@
 import { Route } from '@webiny/react-router'
 import React from 'react'
+import { Home } from '../components/home'
 import { Stocking } from '../components/stocking'
 
 /**
  * When react-router is unable to find a proper route, redirect to "/".
  */
+export const routeTrangChu = {
+  type: 'route',
+  name: 'route-trang-chu',
+  route: <Route path="*" component={Home} />,
+}
+
+
 export const routeNhapHang = {
   type: 'route',
   name: 'route-nhap-hang',

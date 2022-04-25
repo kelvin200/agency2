7064141f3a9b54c6ca28ab364f5617e29afe0367
Kelvin <nhnhatquan@gmail.com>
2022-04-25 12:14:59 +0800

    Update wheel to a workable one with canvas

--- packages/common/frontend/wheel/index.stories.tsx
+++ packages/common/frontend/wheel/index.stories.tsx
@@ -3,6 +3,41 @@ import React from 'react'
 
 import { Wheel } from './index'
 
+const segments = [
+  'better luck next time',
+  'won 70',
+  'won 10',
+  'better luck next time',
+  'won 2',
+  'won uber pass',
+  'better luck next time',
+  'won a voucher',
+  'won 70',
+  'won 10',
+  'better luck next time',
+  'won 2',
+  'won uber pass',
+  'better luck next time',
+  'won a voucher',
+]
+const segColors = [
+  '#EE4040',
+  '#F0CF50',
+  '#815CD1',
+  '#3DA5E0',
+  '#34A24F',
+  '#F9AA1F',
+  '#EC3F3F',
+  '#FF9000',
+  '#F0CF50',
+  '#815CD1',
+  '#3DA5E0',
+  '#34A24F',
+  '#F9AA1F',
+  '#EC3F3F',
+  '#FF9000',
+]
+
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
@@ -19,23 +54,32 @@ const Template: ComponentStory<typeof Wheel> = args => <Wheel {...args} />
 export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  segments: segments,
+  segColors: segColors,
+  winningSegment: segments[3],
+  // onFinished:winner=>onFinished(winner),
+  primaryColor: 'black',
+  contrastColor: 'white',
+  buttonText: 'Spin',
+  isOnlyOnce: true,
+  size: 290,
+  upDuration: 100,
+  downDuration: 1000,
 }
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-  label: 'Button',
-}
+// export const Secondary = Template.bind({})
+// Secondary.args = {
+//   label: 'Button',
+// }
 
-export const Large = Template.bind({})
-Large.args = {
-  size: 'large',
-  label: 'Button',
-}
+// export const Large = Template.bind({})
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// }
 
-export const Small = Template.bind({})
-Small.args = {
-  size: 'small',
-  label: 'Button',
-}
+// export const Small = Template.bind({})
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// }
